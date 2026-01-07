ba9489909f46cc347d6563e95ece3d439b14a9ec
just-jatinverma <vermajatin1727@gmail.com>
2026-01-07 10:00:00 +0530

    ✨ feat(layout): add root layout with authentication and chat sidebar
    - implement root layout component for authenticated pages
    - integrate authentication checks to redirect unauthenticated users to sign-in
    - display chat sidebar for logged-in users within the main application layout
    - provide a main content area for rendering nested route children

---
+++ src/app/(root)/layout.jsx
@@ -0,0 +1,29 @@
+import { auth } from "@/lib/auth";
+import { currentUser } from "@/modules/authentication/actions";
+import ChatSidebar from "@/modules/chat/components/chat-sidebar";
+import { redirect } from "next/dist/server/api-utils";
+import { headers } from "next/headers";
+import React from "react";
+
+const Layout = async ({ children }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  const user = await currentUser();
+
+  if (!session) {
+    return redirect("/sign-in");
+  }
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <ChatSidebar user={user} />
+      <main className="flex-1 overflow-hidden">
+        {/* <Header /> */}
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
