2543e40a85f12002caecb99dbd4dd62bf0432594
just-jatinverma <vermajatin1727@gmail.com>
2026-01-19 10:00:00 +0530

    ✨ feat(chat): add chat detail page
    - create new page for displaying individual chat conversations
    - integrate active chat loader and message view with input form
    - utilize chat id from url parameters to fetch chat data

---
+++ src/app/(root)/chat/[chatId]/page.jsx
@@ -0,0 +1,16 @@
+import ActiveChatLoader from "@/modules/chat/components/active-chat-loader";
+import MessageViewWithForm from "@/modules/chat/components/message-view-form";
+
+const Page = async ({ params }) => {
+  const { chatId } = await params;
+
+  return (
+    <>
+      <ActiveChatLoader chatId={chatId} />
+
+      <MessageViewWithForm chatId={chatId} />
+    </>
+  );
+};
+
+export default Page;
